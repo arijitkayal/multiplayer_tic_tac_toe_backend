c57e0bf6de57707b233aa03929ea82b74db3e9f9
ARIJIT KAYAL <arijit.kayal@yahoo.co.in>
2024-10-24 18:14:31 +0530

    portedit

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ io.on("connection", (socket) => {
     }
   });
 });
-
-myServer.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on port:3000");
+const PORT=process.env.PORT || 3000;
+myServer.listen(PORT, () => {
+  console.log("Server is running on port "+PORT);
 });
